1e555fd4dc5307299d95a9f034820c35198edc39
PerevertenV <pereverten.vadym@gmail.com>
2024-11-06 09:51:54 +0200

    add url genaration

--- UrlShortener/Controllers/HomeController.cs
+++ UrlShortener/Controllers/HomeController.cs
@@ -17,20 +17,82 @@ namespace UrlShortener.Controllers
 	[Authorize]
 	public class HomeController : Controller
 	{
-		private readonly ILogger<HomeController> _logger;
+        private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-		public HomeController(ILogger<HomeController> logger)
-		{
-			_logger = logger;
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
+        {
+            _logger = logger;
+            _unitOfWork = unitOfWork;
 
-		}
+        }
 
-		public IActionResult Index()
-		{
-			return View();
-		}
+        public IActionResult Index()
+        {
+            return View();
+        }
 
-		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult AddCheckShortUrl(int? id) //add and check in one method
+        {
+            var user = HttpContext.User;
+            var userId = user.FindFirstValue("UserID");
+
+            var UsersDomens = _unitOfWork.Domen
+                .GetAll(u => u.UserId == int.Parse(userId));
+
+            if (UsersDomens.Any())
+            {
+                IEnumerable<SelectListItem> DomensList = UsersDomens
+                .Select(u => new SelectListItem
+                {
+                    Text = u.UserDomen,
+                    Value = u.Id.ToString()
+                });
+
+                ViewBag.DomenList = DomensList;
+            }
+            else { ViewBag.DomenList = null; }
+
+            if (id != null && id != 0)
+            {
+                var ShortUrlFromDb = _unitOfWork.Url.GetFirstOrDefault(x => x.Id == id);
+                ShortUrlFromDb.User = _unitOfWork.User.GetFirstOrDefault(x => x.ID == ShortUrlFromDb.UserWhoCreatedUrlId);
+                return View(ShortUrlFromDb);
+            }
+            else
+            {
+                return View(new URL());
+            }
+
+        }
+        [HttpPost]
+        public IActionResult AddCheckShortUrl(URL obj)
+        {
+            List<string> LongUrlListFromDb = _unitOfWork.Url.GetAll()
+                .Select(u => u.LongUrl).ToList();
+            if (LongUrlListFromDb.Contains(obj.LongUrl))
+            {
+                ModelState.AddModelError("LongUrl", "Це URL уже існує");
+                return View(obj);
+            }
+
+            obj.UniqueCode = _unitOfWork.Url.GenerateUniqueCode();
+
+            string domen = obj.domenId == 0 ? StaticData.ShortUrlTemplate : _unitOfWork.Domen
+                .GetFirstOrDefault(u => u.Id == obj.domenId).UserDomen;
+
+            obj.ShortUrl = _unitOfWork.Url.CreateShortUrl(obj.UniqueCode, domen);
+            var user = HttpContext.User;
+            var userId = user.FindFirstValue("UserID");
+            obj.User = _unitOfWork.User.GetFirstOrDefault(u => u.ID == int.Parse(userId));
+            obj.CreatedDate = DateOnly.FromDateTime(DateTime.Now);
+            _unitOfWork.Url.Add(obj);
+            TempData["success"] = "Коротке URL згенеровано успішно";
+            return RedirectToAction(nameof(Index));
+
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 		public IActionResult Error()
 		{
 			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
---
+++ UrlShortener/wwwroot/js/url.js
@@ -0,0 +1,61 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": { url: '/home/getall' },
+        "columns": [
+            {
+                data: 'longUrl',
+                "render": function (data) {
+                    return `<a href="${data}" target="_blank">${data}</a>`;
+                },
+                width: "50%"
+            },
+            {
+                data: 'shortUrl',
+                "render": function (data, type, row) {
+                    return `<a href="${row.longUrl}" target="_blank">${data}</a>`;
+                },
+                width: "30%"
+            },
+            {
+                data: 'id',
+                "render": function (data) {
+                    return `<div class="w-75 btn-group" role="group">
+                        <a href="/home/addCheckShortUrl?id=${data}" class="btn btn-primary mx-2"> <i class="bi bi-info-lg"></i></a>
+                        <a onClick=Delete('/home/delete/${data}') class="btn btn-primary mx-2 btn-danger"> <i class="bi bi-trash3"></i></a>
+                    </div>`;
+                },
+                width: "20%"
+            }
+        ]
+    });
+}
+
+
+function Delete(url) {
+    Swal.fire({
+        title: "Ви впевнені?",
+        text: "Ви не зможете відновити видалине!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Ok"
+    }).then((result) => {
+        if (result.isConfirmed) {
+            $.ajax({
+                url: url,
+                type: 'Delete',
+                success: function (data) {
+                    dataTable.ajax.reload();
+                    toastr.success(data.message);
+                }
+            })
+        }
+    });
+}
